ffa3dfeaea1df52ae7824fbd90da8c732526e842
miquelfarre <miquel_farre@yahoo.es>
2019-02-26 21:26:17 +0100

    first commit

--- dj_challenge_pyspark/invertedIndex.py
+++ dj_challenge_pyspark/invertedIndex.py
@@ -2,6 +2,5 @@ from pyspark import SparkContext
 from hdfs import Config
 
 if __name__ == "__main__":    
-
-println("hdfs python lib to read hadoop directory file, will be processed next, ")
-println("then code logic should be the same than scala module.")
+    print("hdfs python lib to read hadoop directory file, will be processed next, ")
+    print("then code logic should be the same than scala module.")
